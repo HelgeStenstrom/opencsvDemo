045cba1f1e93a794ee5bdd1966a9c32ed1eb33f2
Helge Stenström <h.stenstrom@gmail.com>
2020-10-09 22:44:18 +0200

    comment: testData.csv contents

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -3,6 +3,13 @@ package demo;
 public class Main {
     public static void main(String[] args) {
         String fileName = "/tmp/testData.csv";
+
+/* Contents of the file:
+A,B,C
+1,2,3
+11,12,13
+21,22,23
+*/
         new CsvGetter(fileName).get();
     }
 }
--- src/test/java/demo/CsvGetterTest.java
+++ src/test/java/demo/CsvGetterTest.java
@@ -8,6 +8,14 @@ class CsvGetterTest {
     void testGetter() {
 
         String fileName = "/tmp/testData.csv";
+
+/* Contents of the file:
+A,B,C
+1,2,3
+11,12,13
+21,22,23
+*/
+
         new CsvGetter(fileName).get();
     }
 
